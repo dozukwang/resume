8c88c1febe3c136b2f968bc6d1bea93040a4d5f7
dozukwang <dozukwang@gmail.com>
2024-06-09 20:54:48 +0900

    [chore] 프로젝트 초기세팅 설정

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div>Default Template</div>
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import React from "react"
+
+const root = createRoot(document.getElementById("resume")!)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
+
+// reportWebVitals()
