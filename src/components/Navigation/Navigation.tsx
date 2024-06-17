41ccc17cea84ec82c3531487e30e591dee189ef2
dozukwang <dozukwang@gmail.com>
2024-06-17 20:56:55 +0900

    [feat] Navigation 목록 추가 및 데이터 형식 추가
    - 자기소개 단일에서 기타 항목 추가

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,7 +4,13 @@ import styles from "./Navigation.module.scss"
 
 const Navigation = () => {
     const [section, setSection] = useState<menuNames>("자기소개")
-    const menus: iMenus[] = [{ ref: "introduction", name: "자기소개" }]
+    const menus: iMenus[] = [
+        { ref: "introduction", name: "자기소개" },
+        { ref: "skills", name: "기술스택" },
+        { ref: "work_experience", name: "경력" },
+        { ref: "project", name: "프로젝트" },
+        { ref: "education", name: "교육" }
+    ]
 
     return (
         <div className={styles.navigation}>
--- src/data/components/Navigation/Navigation.d.ts
+++ src/data/components/Navigation/Navigation.d.ts
@@ -1,4 +1,4 @@
-type menuNames = "자기소개"
+type menuNames = "자기소개" | "기술스택" | "경력" | "프로젝트" | "교육"
 
 export interface iMenus {
     ref: string
